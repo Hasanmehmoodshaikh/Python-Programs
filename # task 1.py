f7dbc46c0278e56b25aa76bd422227281de1affb
Hasanmehmoodshaikh <hasanmehmoodshaikh67507@gmail.com>
2024-10-15 21:00:00 +0530

    Add files via upload

---
+++ # task 1.py
@@ -0,0 +1,42 @@
+# task 1
+mylist =['addy','adnan','mayur','suraj','rauf','kezar','abdul','jhon','yogesh','bob']
+print(mylist)
+print("________________________________")
+
+
+# 6
+mylist =['addy','adnan','mayur','suraj','rauf','kezar','abdul','jhon','yogesh','bob']
+print(mylist[5:9])
+print("________________________________")
+
+
+# 7
+mylist =['addy','adnan','mayur','suraj','rauf','kezar','abdul','jhon','yogesh','bob']
+print(mylist[0:7:3])
+print("________________________________")
+
+
+# 8
+mylist =['addy','adnan','mayur','suraj','rauf','kezar','abdul','jhon','yogesh','bob']
+mylist.pop()
+print(mylist)
+print("________________________________")
+
+
+# 9
+student =['addy','adnan','mayur','suraj','rauf','kezar','abdul','jhon','yogesh','bob']
+student.insert(4,'oggy')
+print(student)
+print("________________________________")
+
+
+# 10
+mylist =['addy','adnan','mayur','suraj','rauf','kezar','abdul','jhon','yogesh','bob']
+print(mylist[-3:])
+print("________________________________")
+
+
+# 2
+for i in range(0,10):
+    x =int(input("enter the value "))
+print()
